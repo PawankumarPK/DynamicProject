f3d879c0f1c869148324428767fbcffffcd93270
Pawan Kumar <pk9910765616@gmail.com>
2018-10-16 14:47:39 +0530

    Add retrofit get data in java class

--- app/src/main/java/com/example/hp/dyanamicprojects/MainActivity.java
+++ app/src/main/java/com/example/hp/dyanamicprojects/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.hp.dyanamicprojects;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.example.hp.dyanamicprojects.Api.BloggerApi;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getData();
+    }
+
+    private void getData() {
+        Call<PostList> postList = BloggerApi.getService().getPostList();
+        postList.enqueue(new Callback<PostList>() {
+            @Override
+            public void onResponse(Call<PostList> call, Response<PostList> response) {
+                PostList list = response.body();
+                Toast.makeText(MainActivity.this, "Sucess", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<PostList> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Failure Occured", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
